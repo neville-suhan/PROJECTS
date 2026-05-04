657da52c145bec99bb80b482f197b7280b4d5cda
neville-suhan <nevillesuhanammanna@gmail.com>
2026-05-04 13:37:23 +0530

    second commit

--- Student/connect_mysql.py
+++ Student/connect_mysql.py
@@ -1,17 +1,17 @@
-import mysql.connector
+# import mysql.connector
 
-conn = mysql.connector.connect(
-    host="127.0.0.1",
-    user="root",
-    password="123456",
-    database="student_db",
-    auth_plugin='mysql_native_password'  # 👈 Important
-)
-cursor = conn.cursor()
-cursor.execute("SELECT * FROM students")
+# conn = mysql.connector.connect(
+#     host="127.0.0.1",
+#     user="root",
+#     password="123456",
+#     database="student_db",
+#     auth_plugin='mysql_native_password'  # 👈 Important
+# )
+# cursor = conn.cursor()
+# cursor.execute("SELECT * FROM students")
 
-for row in cursor.fetchall():
-    print(row)
+# for row in cursor.fetchall():
+#     print(row)
 
-cursor.close()
-conn.close()
+# cursor.close()
+# conn.close()
